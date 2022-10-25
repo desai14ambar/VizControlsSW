8d82607935bdfef4963cb842952755652c0630b8
Ambar Desai <ambarish.desai@gm.com>
2022-10-24 20:34:16 -0400

    extract comments working

--- ExtractComments.py
+++ ExtractComments.py
@@ -1,13 +1,15 @@
 from openpyxl import load_workbook
 
 dataFile = 'ExtractFunctions1.xlsx'
+row = 2
+column = 1
 
 wb = load_workbook(filename = dataFile)
 
 # grab the active worksheet
 ws = wb.active
 
-getCell = ws.cell(row=2, column=1) #633
+getCell = ws.cell(row, column) #633
 cellVal = getCell.value
 print(cellVal)
 stringSplit = cellVal.split('\\')
@@ -17,8 +19,9 @@ cppFileName = stringSplit[0]+'.cpp'
 funcName = stringSplit[1]
 print(cppFileName)
 print(funcName)
+ws.cell(row, column+2).value = funcName
 
-getCell = ws.cell(row=2, column=2)
+getCell = ws.cell(row, column+1)
 cellVal = getCell.value
 funcName = (cellVal.partition('\n')[0])
 print(funcName)
@@ -40,24 +43,34 @@ fp.close()
 # print(list_of_lines)
 # for elem in list_of_lines:
 #         print(elem[0])
-t_ype=[]
-stopLim = ['}' , ';' , '#']
-with open(cppFilePath, 'r') as fp:
-    for elem in list_of_lines:
-        l_no = elem[0]
-        for num in reversed(range(l_no)):
-            print(num)
-            line = fp.readline(l_no)
-            # line = line.rstrip()
-            print(line)
-            if any(x in line for x in stopLim):
-                # print(l_no)
-                break
-            else:
-                # print(l_no)
-                t_line = str(line).rstrip('\n')
-                t_ype.append(t_line)
 
-print(t_ype)
-fp.close()
+readFile = open(cppFilePath)
+# read the content of the file opened
+fileContent = readFile.readlines()
+
+arrComments = []
+stopLim = ['}' , ';' , '#']
+for elem in list_of_lines:
+    l_no = elem[0]
+    for num in reversed(range(l_no)):
+        # print(num)
+        line = fileContent[num]
+        # line = line.rstrip()
+        # print(line)
+        if any(x in line for x in stopLim):
+            # print(l_no)
+            break
+        else:
+            # print(l_no)
+            t_line = str(line).rstrip('\n')
+            arrComments.append(t_line)
+
+fileContent = []
+print(arrComments)
+# Using list comprehension + join()
+arrCommentsVal = ' '.join([''.join(sub) for sub in arrComments])
+print(arrCommentsVal)
+ws.cell(row, column+3).value = arrCommentsVal
+
+wb.save(filename = dataFile)
 
